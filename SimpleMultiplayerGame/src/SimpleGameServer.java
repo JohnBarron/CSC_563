5c12e0296ab413d7e0f58f4756b0ce4d7deea98f
JohnBarron <JohnBarron@users.noreply.github.com>
2015-04-28 19:17:26 -0400

    Started a server for a game lobby. A client that I make next can use it to see who else is connected and ready for the game to start.

---
+++ SimpleMultiplayerGame/src/SimpleGameServer.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class SimpleGameServer {
+    private static final int PORT = 4446;
+    private static ArrayList<ClientHandler> clientHandler = new ArrayList<>(3);
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("The simple game server is running.");
+        ServerSocket ss = new ServerSocket(PORT);
+        try {
+            while (true) {
+                clientHandler.add(new ClientHandler(ss.accept(), clientHandler.size()));
+                clientHandler.get(clientHandler.size()-1).start();
+            }
+        } finally {
+            ss.close();
+        }
+    }
+    
+    private static class ClientHandler extends Thread {
+
+        private int playerNum;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+        private boolean ready;
+
+        public ClientHandler(Socket socket, int numPlayers) {
+            this.socket = socket;
+            playerNum = numPlayers;
+            ready = false;
+        }
+
+        public void run() {
+            String input;
+            try {
+                in = new BufferedReader(new InputStreamReader(
+                        socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                
+                while (true) {
+                    input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    if(input.equals("R")){
+                        ready = true;
+                    } else {
+                        ready = false;
+                    }
+                    for (ClientHandler client : clientHandler) {
+                        client.out.println(client.playerNum + " " + client.ready);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+}
